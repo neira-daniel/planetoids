c7266efaedabb463656a6fa89a1c36a1f440e1a5
Daniel Neira <keep.up.the.good.work@outlook.com>
2024-12-17 21:59:19 -0300

    feat: set refresh to 60 FPS

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     # screen is a Surface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -13,6 +15,8 @@ def main():
         screen.fill("black")
         # update the full display Surface
         pygame.display.flip()
+        # set max FPS to 60 Hz
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
