5c793ef079c1472d64586dfde01c73466211df24
Daniel Neira <keep.up.the.good.work@outlook.com>
2024-12-17 23:23:56 -0300

    feat: add a player (triangle) to the screen

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH // 2, y=SCREEN_HEIGHT // 2)
     # screen is a Surface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # fill is a Surface method
         screen.fill("black")
+        # draw player
+        player.draw(screen)
         # update the full display Surface
         pygame.display.flip()
         # set max FPS to 60 Hz
