cc80cd7395501ae34511a2d3e9dd081de94112d2
Daniel Neira <keep.up.the.good.work@outlook.com>
2024-12-18 01:12:14 -0300

    refactor: use pygame's group logic to manage the player(s)

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    # group logic
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    # player(s)
     player = Player(x=SCREEN_WIDTH // 2, y=SCREEN_HEIGHT // 2)
     # screen is a Surface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,10 +21,12 @@ def main():
                 return
         # fill is a Surface method
         screen.fill("black")
-        # update player position
-        player.update(dt)
-        # draw player
-        player.draw(screen)
+        # update position(s) of updatable Player's instances
+        for thing in updatable:
+            thing.update(dt)
+        # draw drawable Player's instances
+        for thing in drawable:
+            thing.draw(screen)
         # update the full display Surface
         pygame.display.flip()
         # set max FPS to 60 Hz
