e64efb8248ebf10403cce6606df009bd1b68f722
Daniel Neira <keep.up.the.good.work@outlook.com>
2024-12-18 00:58:22 -0300

    feat: add ability to move the player forward and backward on screen

--- player.py
+++ player.py
@@ -28,6 +28,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
     # method given by the instructor
     def move(self, dt):
